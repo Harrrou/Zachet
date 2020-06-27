de0554f91f31290b9fe7e32dce640e0fe9731c85
Влад Караев <vlad-karaev-2014@mail.ru>
2020-06-28 00:11:07 +0400

    Добавьте файлы проекта.

---
+++ zachet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zachet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+            int summ = 0;
+            foreach (char c in str)
+            {
+                if (Char.IsDigit(c) == true)
+                    summ += int.Parse(c.ToString());
+            }
+            Console.Write("Сумма чисел в строке = " + summ);
+            Console.Read();
+        }
+    }
+}
